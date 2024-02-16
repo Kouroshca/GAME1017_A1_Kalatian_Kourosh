9fc212becebc2d33eabb4be9ba6b51c74b33f20c
Kouroshca <156234772+Kouroshca@users.noreply.github.com>
2024-02-16 16:06:05 -0500

    movement of enemy fixed
    movement enemy fixed

--- Assets/movement/enemymovement.cs
+++ Assets/movement/enemymovement.cs
@@ -5,14 +5,27 @@ using UnityEngine;
 public class enemymovement : MonoBehaviour
 {
     // Start is called before the first frame update
-   [SerializeField] Transform Player;
+    [SerializeField] Transform Player;
+    [SerializeField] float speed;
+
+    void Awake()
+    {
+        // Find and reference the transform of the player when the enemy is spawned.
+
+        // Fınd by the movement compenent
+        // Player = GameObject.FindAnyObjectByType<MovementNew>().transform;
+
+        // Find by name
+        Player = GameObject.Find("Player").transform;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(transform.position, Player.position)< 8.0f)
+        if (Vector2.Distance(transform.position, Player.position) < 8.0f)
         {
-           transform.Translate(Vector3.left * 1.0f *Time.deltaTime);
-           
+            
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
         }
     }
 }
