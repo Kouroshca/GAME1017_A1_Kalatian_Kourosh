872f58f02ccf1c350dc7da454b824aa702a26b4c
Kouroshca <156234772+Kouroshca@users.noreply.github.com>
2024-03-07 00:30:59 -0500

    ProjectAlmostDone
    Enemy shooting is fixed
    Remaining: scrolling background

--- Assets/C# script/NotForMilestone/Projectile2.cs
+++ Assets/C# script/NotForMilestone/Projectile2.cs
@@ -8,11 +8,17 @@ public class Projectile2 : MonoBehaviour
 {
     // Start is called before the first frame update
       public float speed = 7.0f; // meter per second
+      public float range = 10;
       
        // Update is called once per frame
     void Update()
     {
         float distance = speed * Time.deltaTime;
         transform.Translate(-transform.right * distance);
+        if(Vector3.Distance(transform.position, transform.parent.position)>=range)
+        {
+            Destroy(gameObject);
+        }
+    transform.Rotate(Vector3.forward,180.0f*Time.deltaTime);
     }
 }
--- Assets/C# script/NotForMilestone/enemyshoot.cs
+++ Assets/C# script/NotForMilestone/enemyshoot.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class enemyshoot : MonoBehaviour
@@ -9,7 +10,7 @@ public class enemyshoot : MonoBehaviour
     public Transform blasterposition;
     public float shootinterval = 3.0f;
     
-    public float firrate = 1.333f;
+    public float firrate = 0.5f;
    public float shoottimer = 0.0f;
     void Start()
     {
@@ -21,7 +22,7 @@ public class enemyshoot : MonoBehaviour
     {
         
        shoottimer += Time.deltaTime;
-       if(shoottimer >=1.0f/firrate)
+       if(shoottimer >=1.0f)
        {
         Shoot();
         shoottimer = 0.0f;
@@ -29,10 +30,12 @@ public class enemyshoot : MonoBehaviour
     }
     public void Shoot(){
 
-     Quaternion rotation = blasterposition.rotation;
-     
-    Quaternion Rotation = blasterposition.rotation;
-    Instantiate(projectileprefab,blasterposition.position, rotation);
-
+     //Quaternion rotation = Quaternion.Euler(0.0f,0f,90f);
+     //Quaternion Rotation = blasterposition.rotation;
+    //Instantiate(projectileprefab,blasterposition.position, rotation);
+    Quaternion rotation = blasterposition.rotation;
+    GameObject projectileSpawn = Instantiate(projectileprefab,blasterposition.position,rotation);
+    Rigidbody2D projectileRb = projectileSpawn.GetComponent<Rigidbody2D>();
+    projectileRb.velocity =  new Vector3(-10.0f,0.0f);
     }
 }
